7d1a399c0d761773042c0f15f2cc628a7f86a671
Ayush Gupta <74704957+ayush-921@users.noreply.github.com>
2023-08-09 09:06:20 +0530

    settings update

--- core/settings.py
+++ core/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'djangoecom.noreply@gmail.com'
-# EMAIL_HOST_PASSWORD = 'django20168' #past the key or password app here
+# EMAIL_HOST_USER = 'sender email'
+# EMAIL_HOST_PASSWORD = 'sender password' #past the key or password app here
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # DEFAULT_FROM_EMAIL = 'default from email'
